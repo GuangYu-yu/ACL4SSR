2479054d49f59198e18fe540f4c22925bd1bbad9
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2024-09-12 03:02:13 +0800

    Update extract_cidr.py

--- extract_cidr.py
+++ extract_cidr.py
@@ -1,4 +1,5 @@
 import maxminddb
+import ipaddress
 
 # 定义目标地区的ISO代码
 target_countries = {
@@ -14,15 +15,25 @@ country_cidrs = {country_code: [] for country_code in target_countries}
 
 # 读取 GeoLite2 数据库
 with maxminddb.open_database('GeoLite2-Country.mmdb') as reader:
-    for ip in reader:
-        country_data = reader.get(ip)
+    # 遍历 IPv4 地址范围
+    for ip in ipaddress.IPv4Network("0.0.0.0/0").hosts():
+        country_data = reader.get(str(ip))
         if country_data and 'country' in country_data:
             country_name = country_data['country']['names']['en']
             for code, name in target_countries.items():
                 if country_name == name:
-                    cidr = ip  # 获取对应的 CIDR
-                    if ':' in cidr:  # 区分 IPv6
-                        country_cidrs[code].append(cidr)
+                    cidr = f"{ip}/32"
+                    country_cidrs[code].append(cidr)
+    
+    # 遍历 IPv6 地址范围
+    for ip in ipaddress.IPv6Network("::/0").hosts():
+        country_data = reader.get(str(ip))
+        if country_data and 'country' in country_data:
+            country_name = country_data['country']['names']['en']
+            for code, name in target_countries.items():
+                if country_name == name:
+                    cidr = f"{ip}/128"
+                    country_cidrs[code].append(cidr)
 
 # 确保将结果写入到 /Clash 目录中
 for code, cidrs in country_cidrs.items():
