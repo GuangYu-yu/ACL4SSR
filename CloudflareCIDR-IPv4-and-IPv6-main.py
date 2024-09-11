2b1050d09e9c90dd8f2206a7d5454fa84beb1941
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2024-09-11 22:13:06 +0800

    Update CloudflareCIDR-IPv4-and-IPv6-main.py

--- CloudflareCIDR-IPv4-and-IPv6-main.py
+++ CloudflareCIDR-IPv4-and-IPv6-main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import zipfile
 import requests
+import ipaddress
 
 # 下载zip文件
 url = "https://github.com/ipverse/asn-ip/archive/refs/heads/master.zip"
@@ -40,14 +41,22 @@ for root, dirs, files in os.walk("asn-ip-master/as"):
                     if not ip.startswith('#'):
                         ipv6_addresses.append(ip)
 
-# 将IPv4结果写入一个新的文件
+# 将字符串转换为IPv4/IPv6网络对象
+ipv4_networks = [ipaddress.IPv4Network(ip, strict=False) for ip in ipv4_addresses]
+ipv6_networks = [ipaddress.IPv6Network(ip, strict=False) for ip in ipv6_addresses]
+
+# 合并CIDR范围并排序
+ipv4_merged_sorted = sorted(ipaddress.collapse_addresses(ipv4_networks), key=lambda net: (net.network_address, net.prefixlen))
+ipv6_merged_sorted = sorted(ipaddress.collapse_addresses(ipv6_networks), key=lambda net: (net.network_address, net.prefixlen))
+
+# 将合并并排序后的IPv4结果写入文件
 with open('Clash/CloudflareCIDR.txt', 'w') as file:
-    for ip in ipv4_addresses:
+    for ip in ipv4_merged_sorted:
         file.write(f"{ip}\n")
 
-# 将IPv6结果写入一个新的文件
+# 将合并并排序后的IPv6结果写入文件
 with open('Clash/CloudflareCIDR-v6.txt', 'w') as file:
-    for ip in ipv6_addresses:
+    for ip in ipv6_merged_sorted:
         file.write(f"{ip}\n")
 
 # 清理下载的zip文件和解压的文件夹
