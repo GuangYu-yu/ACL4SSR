088b05b1fe539266bfdfb1a70a9f30b7a3676522
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2025-10-14 19:00:40 +0800

    Update 规则剔除Cloudflare.py

--- 规则剔除Cloudflare.py
+++ 规则剔除Cloudflare.py
@@ -7,6 +7,7 @@ gfw_list_url = "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/
 
 # 输出文件名
 output_file = "global_cf_filtered.list"
+cf_direct_file = "cf_direct.list"
 
 def fetch_list(url):
     """获取列表并去掉空行和注释"""
@@ -23,10 +24,16 @@ def main():
     # 从 cloudflare_domains 中剔除 gfw_list 条目
     filtered_cloudflare = [domain for domain in cloudflare_domains if domain not in gfw_list]
 
+    # 写入 cf_direct.list
+    with open(cf_direct_file, "w", encoding="utf-8") as f:
+        for line in filtered_cloudflare:
+            f.write(line + "\n")
+    print(f"完成，生成文件: {cf_direct_file}")
+
     # 从 Global.list 中剔除 filtered_cloudflare 条目
     final_list = [item for item in global_list if item not in filtered_cloudflare]
 
-    # 写入文件
+    # 写入 global_cf_filtered.list
     with open(output_file, "w", encoding="utf-8") as f:
         for line in final_list:
             f.write(line + "\n")
